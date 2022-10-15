c2c6a125a64f7d83f9ecd766c8dfbd9ce80584d3
Sanghak Kim <k3hppk@gmail.com>
2022-10-15 16:34:57 +0900

    계산하는 기능 추가하기

--- src/calculatorModel.js
+++ src/calculatorModel.js
@@ -16,6 +16,21 @@ class CalculatorModel {
   divide(firstNum, secondNum) {
     return firstNum / secondNum;
   }
+
+  operate(firstNum, secondNum, operator) {
+    switch (operator) {
+      case "add":
+        return this.add(firstNum, secondNum);
+      case "subtract":
+        return this.subtract(firstNum, secondNum);
+      case "multiply":
+        return this.multiply(firstNum, secondNum);
+      case "divide":
+        return this.divide(firstNum, secondNum);
+      default:
+        throw new Error("잘못된 연산자입니다.");
+    }
+  }
 }
 
 export { CalculatorModel };
