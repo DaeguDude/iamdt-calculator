adc0650194cc78b7b566b7f6e518173f51aa7765
Sanghak Kim <k3hppk@gmail.com>
2022-10-15 16:25:10 +0900

    사칙연산 기능구현하기

---
+++ src/calculatorModel.js
@@ -0,0 +1,21 @@
+class CalculatorModel {
+  constructor() {}
+
+  add(firstNum, secondNum) {
+    return firstNum + secondNum;
+  }
+
+  subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+  }
+
+  multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+  }
+
+  divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+  }
+}
+
+export { CalculatorModel };
