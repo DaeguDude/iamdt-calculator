3fdbf4972a8f5f135c85bd837345e41d393c3361
Sanghak Kim <k3hppk@gmail.com>
2022-10-16 21:03:48 +0900

    기본적인 이벤트 핸들러 연동하기

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { CalculatorModel } from "./calculatorModel";
+import { View } from "./view";
 import "./style.css";
+
+const view = new View(new CalculatorModel());
+view.addEventHandlers();
