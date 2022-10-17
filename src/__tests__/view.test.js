82c1792cf5ff40c708359e20e11f18d2e7bac6a4
Sanghak Kim <k3hppk@gmail.com>
2022-10-17 11:05:37 +0900

    사칙연산, 소수점에 대해서 테스트를 추가합니다

--- src/__tests__/view.test.js
+++ src/__tests__/view.test.js
@@ -36,7 +36,7 @@ const getHtml = () =>
       <div class="button-wrapper"><button id="plus" class="orange operator" data-operator="+">+</button></div>
     </div>
     <div class="button-row">
-      <div id="zero-wrapper" class="button-wrapper"><button id="num0" class="number dark-grey " data-num-value="0">0</button></div>
+      <div id="zero-wrapper" class="button-wrapper"><button id="num-0" class="number dark-grey " data-num-value="0">0</button></div>
       <div class="button-wrapper"><button id="decimal" class="dark-grey">.</button></div>
       <div class="button-wrapper"><button id="equal" class="orange">=</button></div>
     </div>
@@ -133,5 +133,159 @@ describe("percent", () => {
     expect(document.querySelector("#display-text").innerText).toBe(
       "0.000000000003"
     );
+  });
+});
+
+describe("소수점(decimal)", () => {
+  test("소수점을 클릭했을 시 화면에 제대로 나타납니다", () => {
+    document.body.innerHTML = getHtml();
+    const view = new View(new CalculatorModel());
+    view.addEventHandlers();
+
+    document.querySelector("#num-5").click();
+    document.querySelector("#decimal").click();
+
+    expect(document.querySelector("#display-text").innerText).toBe("5.");
+  });
+  test("소수점을 여러 번 클릭해도 화면에 한 번만 나타납니다", () => {
+    document.body.innerHTML = getHtml();
+    const view = new View(new CalculatorModel());
+    view.addEventHandlers();
+
+    document.querySelector("#num-5").click();
+    document.querySelector("#decimal").click();
+    document.querySelector("#decimal").click();
+    document.querySelector("#decimal").click();
+    document.querySelector("#decimal").click();
+
+    expect(document.querySelector("#display-text").innerText).not.toBe("5....");
+    expect(document.querySelector("#display-text").innerText).toBe("5.");
+  });
+});
+
+describe("사칙연산", () => {
+  describe("더하기", () => {
+    test("5 + 5 = 10", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-5").click();
+      document.querySelector("#plus").click();
+      document.querySelector("#num-5").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(10);
+    });
+
+    test("3.2 + 4.5 = 7.7", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-3").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-2").click();
+      document.querySelector("#plus").click();
+      document.querySelector("#num-4").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-5").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(7.7);
+    });
+  });
+
+  describe("빼기", () => {
+    test("5 - 10 = -5", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-5").click();
+      document.querySelector("#minus").click();
+      document.querySelector("#num-1").click();
+      document.querySelector("#num-0").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(-5);
+    });
+
+    test("1.63 - 2.63 = -1", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-1").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-6").click();
+      document.querySelector("#num-3").click();
+
+      document.querySelector("#minus").click();
+
+      document.querySelector("#num-2").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-6").click();
+      document.querySelector("#num-3").click();
+
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(-1);
+    });
+  });
+
+  describe("나누기", () => {
+    test("5 / 1 = 5", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-5").click();
+      document.querySelector("#divide").click();
+      document.querySelector("#num-1").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(5);
+    });
+
+    test("1.5 / 3 = 0.5", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-1").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-5").click();
+      document.querySelector("#divide").click();
+      document.querySelector("#num-3").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(0.5);
+    });
+  });
+
+  describe("곱하기", () => {
+    test("10 * 21 = 210", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-1").click();
+      document.querySelector("#num-0").click();
+      document.querySelector("#multiply").click();
+      document.querySelector("#num-2").click();
+      document.querySelector("#num-1").click();
+      document.querySelector("#equal").click();
+
+      expect(document.querySelector("#display-text").innerText).toBe(210);
+    });
+
+    test("1.5 * 3 = 4.5", () => {
+      document.body.innerHTML = getHtml();
+      const view = new View(new CalculatorModel());
+      view.addEventHandlers();
+
+      document.querySelector("#num-1").click();
+      document.querySelector("#decimal").click();
+      document.querySelector("#num-5").click();
+      document.querySelector("#multiply").click();
+      document.querySelector("#num-3").click();
+      document.querySelector("#equal").click();
+      expect(document.querySelector("#display-text").innerText).toBe(4.5);
+    });
   });
 });
